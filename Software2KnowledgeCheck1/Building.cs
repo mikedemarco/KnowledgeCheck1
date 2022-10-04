a8cfeb1a2f4c711ea2bb8101b91b5ad4721a4a60
Michael De Marco <105325093+mikedemarco@users.noreply.github.com>
2022-10-04 19:40:07 -0400

    Add files via upload

---
+++ Software2KnowledgeCheck1/Building.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class Building
+    {
+        public string Street { get; set; }
+
+        public int StreetNumber { get; set; }
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+{
+    if (permit && zoning)
+    {
+        foreach (var material in materials)
+        {
+            if (material == "concrete")
+            {
+                // start laying foundation
+            }
+            else if (material == "Steel")
+            {
+                // Start building structure
+            }
+            // etc etc...
+
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+
+        if (buildingWasMade)
+{
+    Buildings.Add(apartment);
+}
